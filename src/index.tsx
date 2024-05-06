1097b8dffedac348a3de69106efae875a164ce5e
dengsq <dengsq@suancloud.cn]>
2024-05-06 14:23:24 +0800

    feat: init

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './style/border.css';
+import 'normalize.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 * 100 + 'px'
+
+window.addEventListener('resize',()=>{
+  document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 * 100 + 'px'
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,8 +18,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
